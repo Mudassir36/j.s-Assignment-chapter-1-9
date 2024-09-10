c08e4d27c800ce04139da6a09889cf5aef720145
Mudassir36 <mk5208694@gmail.com>
2024-09-10 19:55:24 +0500

    Add files via upload

---
+++ Assignment/app.js
@@ -0,0 +1,322 @@
+// // Chapter 1
+
+// // Q1
+// alert("Error! please inter a valid password");
+
+
+
+
+
+// // Q2
+// alert("Welcome to js Land.. Happy Coding");
+
+
+
+
+// // Q3
+// alert("Welcome to js Land..");
+
+
+
+// // Q4
+// alert("Happy Coding! prevent this page frome craeting additional dailogs.");
+
+
+
+// // Q5
+// alert("Hello... I can run JS through my web browser's console");
+
+
+                   // Cheptor  2 //
+
+
+
+
+// Q1
+// var username ="mudassir rasheed";
+// alert(username)
+
+
+
+// Q2
+// var myAge ="22 year old";
+// alert(myAge)
+
+
+
+// Q3
+// var titeld ="Certified Mobile Application development";
+// alert(titeld)
+
+
+// Q4
+// var food="PIZZA";
+// food="PIZZ";
+// food="PIZ";
+// food="PI";
+// food="P";
+// alert(food)
+
+
+
+// Q5
+// var myEmail="mudassir8694@gmail.com";
+// alert(myEmail)
+
+
+
+// Q6
+// var book="I am traying to learn from the Book A smarter way to learn JavaScript";
+// alert(book)              
+
+
+
+
+        // chapter   3  //
+
+
+
+// Q1
+// var age="i am 22 year old";
+// alert(age)
+
+// Q2
+// var visitor="You have visited this site 14 times";
+// alert(visitor)
+
+
+
+
+
+          // chapter 5   //
+
+
+// Q1
+// var num="5";
+// // var addNum="3";
+// alert(3 + 5)
+
+
+
+// Q2
+// var numb="13";
+// alert(numb)
+
+// numb =numb - 1
+// alert(numb)
+
+// numb= numb - 12
+// alert(numb)
+
+
+
+// Q3
+// var num=5;
+// alert(num)
+
+// num++
+// alert(num)
+
+// num= num + 7;
+// alert(num)
+
+// --num
+// alert(num)
+// num =num - 12
+// alert(num)
+
+
+// Q3
+// var num=600
+// alert(num)
+
+// num=num * 5
+// alert(num)
+
+// var num= "total 5 tickets 1 tecket 3000PKR";
+// alert(num)
+
+// num= 3000*5
+// alert(num)
+
+
+
+// Q4
+// var num=4
+// alert(num)
+
+// num=num * 2
+// alert(num)
+
+// num=4 * 3
+// alert(num)
+
+// num=4 * 4
+// alert(num)
+
+// num=4 * 5
+// alert(num)
+
+// num=4 * 6
+// alert(num)
+
+// num=4 * 7
+// alert(num)
+
+// num=4 * 8
+// alert(num)
+
+// num=4 * 9
+// alert(num)
+
+// num=4 * 10
+// alert(num)
+
+
+
+            // chapter  6  //
+
+            
+// document.write("<h1>hello world</h1>");
+
+
+
+// cheptor 6
+
+
+// Q1
+// var a=10
+// alert(a)
+
+// a++
+// alert(a)
+
+// ++a
+// alert(a)
+
+// --a
+// alert(a)
+
+// a--
+// alert(a)
+
+
+
+
+// Q2
+// var a=2
+// var b=1
+// alert(--a - --b + ++b + b--)
+
+// --a
+// alert(a)
+
+// alert(--a - --b)
+
+// alert(--a - --b + ++b + b--)
+
+
+
+// Q3
+// var a=156
+// var b=100
+// var c=300
+// alert(a * b / c )
+
+
+   // chapter 9 //
+
+
+// cheptor 9
+
+
+// Q1
+// var city= prompt("Enter your city name");
+// if(city === "karachi"){
+//     alert("Wellcome To The City Of Lights")
+// console.log(city);
+
+
+// Q2
+// var gender = prompt("Enter your gendar");
+// if(gender === "male"){
+//     alert("Good Morning Sir")
+// }
+// if(gender === "female"){
+//     alert("Good Morning Ma'me")
+// }
+
+
+// Q3
+// var signalColor = prompt("Enter The Color");
+// if (signalColor === "Red"){
+//     alert("Must Stop");
+// }
+// if (signalColor === "Yellow"){
+//     alert("Readyt To Move");
+// }
+// if (signalColor === "Green"){
+//     alert("Move Now");
+// }
+
+
+
+// Q4
+// var fuel = +prompt("imput Litres")
+// var liters = 1
+// if (fuel < liters){
+//     alert("Please Refil The fuel in your car")
+// }
+// if (fuel >= liters){
+//     alert("Correct !")
+// }
+
+
+// Q5
+// var  = prompt("Enter The Number");
+// var a = prompt("Enter the number")
+// var a = 2;
+// if (++a === 3){
+//     alert("givin condition for variable a is true");
+// }
+
+// var b = prompt("Enter the number")
+// var b = 82
+// if (++b === 83){
+//     alert("givin condition for variable b is true");
+// }
+
+// var c = prompt("Enter the number")
+// var c = 12
+// if (++c === 13){
+//     alert("Condition 1 is true");
+// }
+
+// if (c === 13){
+//     alert("Condition 2 is true")
+// }
+// if (++c < 14){
+//     alert("Condition 3 is true")
+// }
+// if (c === 14){
+//     alert("Condition 4 is true")
+// }
+
+
+
+// Q6
+// var first = +prompt("Enter The number");
+// var second = +prompt("Enter The number")
+// var total = first + second
+// if (total <= 20000){
+//     alert("Correct !")
+// } else {
+//     alert("Errore")
+// }
+
+
+
+
+
+
+
